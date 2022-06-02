9fc27da88de58797da4752fe552171bc9fb6707c
Mabast <mabast.niga@yahoo.com>
2022-06-02 14:14:18 -0500

    Added handle close functionality for  sidebar and fixed issue with navbar icon sizes

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,8 +12,9 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
 
-  const handleClick = ({ clicked }) => {
+  const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true });
   };
 
@@ -25,6 +26,8 @@ export const ContextProvider = ({ children }) => {
         isClicked,
         setIsClicked,
         handleClick,
+        screenSize,
+        setScreenSize,
       }}
     >
       {children}

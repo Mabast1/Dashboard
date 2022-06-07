9a6ca8583c180b7a32cc9f1d12ebefde64177181
Mabast <mabast.niga@yahoo.com>
2022-06-07 00:33:21 -0500

    Created stacked chart page using stacked component

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+import { Header } from "../../components";
+import { Stacked } from "../../components";
+
+const StackedPage = () => {
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl ">
+      <Header category="Chart" title="Revenue Breakdown" />
+      <Stacked />
+    </div>
+  );
 };
 
-export default Stacked;
+export default StackedPage;

4fff8ac15756c49a4c25922cca5628558a802f34
Mabast <mabast.niga@yahoo.com>
2022-06-08 16:27:39 -0500

    created carts component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+
+import { useStateContext } from "../contexts/ContextProvider";
+import { cartData } from "../data/dummy";
+import { Button } from "./";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { currentColor } = useStateContext();
+
+  return (
+    <div className=" bg-half-transparent w-full fixed nav-item top-0 right-0">
+      <div className=" float-right h-screen duration-1000 ease-in-out dark:text-gray-200 transition-all dark:bg-[#484852] bg-white md:w-400 p-8">
+        <div className="flex justify-between items-center">
+          <p className=" font-semibold text-lg">Shopping Cart</p>
+          <Button
+            icon={<MdOutlineCancel />}
+            // bgColor={currentColor}
+            color="rgb(153, 171, 180)"
+            borderRadius="50%"
+            size="2xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;

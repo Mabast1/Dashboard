997650790ba03ce8fc13ba1c7a17f960addee1ab
Mabast <mabast.niga@yahoo.com>
2022-06-09 17:22:24 -0500

    Added button to see all msgs to chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,7 +6,7 @@ import { chatData } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Chat = () => {
-  const { currentMode } = useStateContext();
+  const { currentColor } = useStateContext();
 
   return (
     <div className="nav-item absolute right-5 md:right-52 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96 shadow-md">
@@ -55,6 +55,15 @@ const Chat = () => {
             </div>
           </div>
         ))}
+        <div className="mt-5">
+          <Button
+            color="white"
+            bgColor={currentColor}
+            text="See all messages"
+            borderRadius="10px"
+            width="full"
+          />
+        </div>
       </div>
     </div>
   );
